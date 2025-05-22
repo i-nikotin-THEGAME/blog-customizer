9eae8293e0ee57383a60f7423d7b4646115ca868
i-nikotin-THEGAME <i_nikotin@thegame.design>
2025-05-22 23:29:25 +0300

    Добавил onChange к каждому Select и RadioGroup

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -7,17 +7,31 @@ import { useState } from 'react';
 import { Text } from 'src/ui/text';
 import { Select } from 'src/ui/select';
 import { RadioGroup } from 'src/ui/radio-group';
-import { backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
 import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+    state: ArticleStateType;
+    onStateChange: (newState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({state, onStateChange}: ArticleParamsFormProps) => {
 	const [open, setOpen] = useState(false);
+
+	const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Будет логика при применении изменений
+    };
+
+    const handleReset = () => {
+        onStateChange(defaultArticleState);
+    };
 
 	return (
 		<>
 			<ArrowButton isOpen={open} onClick={() => setOpen(!open)} />
 			<aside className={styles.container + ' ' + (open ? styles.container_open : '')}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
 					<Text 
 					children='ЗАДАЙТЕ ПАРАМЕТРЫ'
 					as='h2'
@@ -26,12 +40,42 @@ export const ArticleParamsForm = () => {
 					uppercase={true}
 					align='center'
 					/>
-					< Select selected={fontFamilyOptions[0]} options={fontFamilyOptions} title='Шрифт'/>
-					< RadioGroup name='font' options={fontSizeOptions} selected={fontSizeOptions[0]} title='рАЗМЕР шрифта'/>
-					< Select selected={fontColors[0]}  options={fontColors} title='Цвет шрифта'/>
+					<Select 
+					selected={state.fontFamilyOption} 
+					options={fontFamilyOptions} 
+					title='Шрифт'
+					placeholder='Выбирете шрифт'
+					onChange={(option) => onStateChange({...state, fontFamilyOption: option})}
+					/>
+					<RadioGroup 
+					name='font' 
+					options={fontSizeOptions} 
+					selected={state.fontSizeOption} 
+					title='рАЗМЕР шрифта'
+					onChange={(option) => onStateChange({...state, fontSizeOption: option})}
+					/>
+					<Select 
+					selected={state.fontColor}  
+					options={fontColors} 
+					title='Цвет шрифта'
+					placeholder='Выбирете цыет шрифта'
+					onChange={(option) => onStateChange({...state, fontColor: option})}
+					/>
 					<Separator />
-					< Select selected={backgroundColors[0]}  options={backgroundColors} title='Цвет фона'/>
-					< Select selected={contentWidthArr[0]}  options={contentWidthArr} title='Ширина контента'/>
+					<Select 
+					selected={state.backgroundColor}  
+					options={backgroundColors} 
+					title='Цвет фона'
+					placeholder='Выбирете цыет фона'
+					onChange={(option) => onStateChange({...state, backgroundColor: option})}
+					/>
+					<Select 
+					selected={state.contentWidth}  
+					options={contentWidthArr} 
+					title='Ширина контента'
+					placeholder='Выбирете ширину контента'
+					onChange={(option) => onStateChange({...state, contentWidth: option})}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
