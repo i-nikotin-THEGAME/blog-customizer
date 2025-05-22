458e8c16d51c2647f5250b664ad0f46e28388de1
i-nikotin-THEGAME <i_nikotin@thegame.design>
2025-05-23 01:45:41 +0300

    Доработана функицональность закрытия компонента ArticleParamsForm за ее пределами

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,80 +1,126 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
-
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Text } from 'src/ui/text';
 import { Select } from 'src/ui/select';
 import { RadioGroup } from 'src/ui/radio-group';
-import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { Separator } from 'src/ui/separator';
 
 type ArticleParamsFormProps = {
-    state: ArticleStateType;
-    onStateChange: (newState: ArticleStateType) => void;
+	state: ArticleStateType;
+	onStateChange: (newState: ArticleStateType) => void;
 };
 
-export const ArticleParamsForm = ({state, onStateChange}: ArticleParamsFormProps) => {
+export const ArticleParamsForm = ({
+	state,
+	onStateChange,
+}: ArticleParamsFormProps) => {
 	const [open, setOpen] = useState(false);
+	const [formState, setFormState] = useState<ArticleStateType>(state);
+	const asideRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (e: MouseEvent) => {
+			if (asideRef.current && !asideRef.current.contains(e.target as Node)) {
+				setOpen(false);
+			}
+		};
+
+		if (open) {
+			document.addEventListener('mousedown', handleClickOutside);
+		}
+
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [open]);
 
 	const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        // Будет логика при применении изменений
-    };
+		e.preventDefault();
+		onStateChange(formState);
+	};
 
-    const handleReset = () => {
-        onStateChange(defaultArticleState);
-    };
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onStateChange(defaultArticleState);
+	};
+
+	const toggleForm = () => {
+		setOpen(!open);
+	};
 
 	return (
 		<>
-			<ArrowButton isOpen={open} onClick={() => setOpen(!open)} />
-			<aside className={styles.container + ' ' + (open ? styles.container_open : '')}>
-				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
-					<Text 
-					children='ЗАДАЙТЕ ПАРАМЕТРЫ'
-					as='h2'
-					size={31}
-					weight={800}
-					uppercase={true}
-					align='center'
+			<ArrowButton isOpen={open} onClick={toggleForm} />
+			<aside
+				className={styles.container + ' ' + (open ? styles.container_open : '')}
+				ref={asideRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text
+						children='ЗАДАЙТЕ ПАРАМЕТРЫ'
+						as='h2'
+						size={31}
+						weight={800}
+						uppercase
+						align='center'
 					/>
-					<Select 
-					selected={state.fontFamilyOption} 
-					options={fontFamilyOptions} 
-					title='Шрифт'
-					placeholder='Выбирете шрифт'
-					onChange={(option) => onStateChange({...state, fontFamilyOption: option})}
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						placeholder='Выбирете шрифт'
+						onChange={(option) =>
+							setFormState({ ...formState, fontFamilyOption: option })
+						}
 					/>
-					<RadioGroup 
-					name='font' 
-					options={fontSizeOptions} 
-					selected={state.fontSizeOption} 
-					title='рАЗМЕР шрифта'
-					onChange={(option) => onStateChange({...state, fontSizeOption: option})}
+					<RadioGroup
+						name='font'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						title='рАЗМЕР шрифта'
+						onChange={(option) =>
+							setFormState({ ...formState, fontSizeOption: option })
+						}
 					/>
-					<Select 
-					selected={state.fontColor}  
-					options={fontColors} 
-					title='Цвет шрифта'
-					placeholder='Выбирете цыет шрифта'
-					onChange={(option) => onStateChange({...state, fontColor: option})}
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+						placeholder='Выбирете цыет шрифта'
+						onChange={(option) =>
+							setFormState({ ...formState, fontColor: option })
+						}
 					/>
 					<Separator />
-					<Select 
-					selected={state.backgroundColor}  
-					options={backgroundColors} 
-					title='Цвет фона'
-					placeholder='Выбирете цыет фона'
-					onChange={(option) => onStateChange({...state, backgroundColor: option})}
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+						placeholder='Выбирете цыет фона'
+						onChange={(option) =>
+							setFormState({ ...formState, backgroundColor: option })
+						}
 					/>
-					<Select 
-					selected={state.contentWidth}  
-					options={contentWidthArr} 
-					title='Ширина контента'
-					placeholder='Выбирете ширину контента'
-					onChange={(option) => onStateChange({...state, contentWidth: option})}
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						placeholder='Выбирете ширину контента'
+						onChange={(option) =>
+							setFormState({ ...formState, contentWidth: option })
+						}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
